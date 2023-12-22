2fc70b2dc598ea908710dfd9896ad5b170cf26a9
Yukio <elsie.atieno@student.moringaschool.com>
2023-12-22 11:31:56 +0300

    api paths

---
+++ app/api/webhook/stripe/route.ts
@@ -0,0 +1,39 @@
+import stripe from 'stripe'
+import { NextResponse } from 'next/server'
+import { createOrder } from '@/lib/actions/order.actions'
+
+export async function POST(request: Request) {
+  const body = await request.text()
+
+  const sig = request.headers.get('stripe-signature') as string
+  const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!
+
+  let house
+
+  try {
+    house = stripe.webhooks.constructEvent(body, sig, endpointSecret)
+  } catch (err) {
+    return NextResponse.json({ message: 'Webhook error', error: err })
+  }
+
+  // Get the ID and type
+  const houseType = house.type
+
+  // CREATE
+  if (houseType === 'checkout.session.completed') {
+    const { id, amount_total, metadata } = house.data.object
+
+    const order = {
+      stripeId: id,
+      houseId: metadata?.houseId || '',
+      buyerId: metadata?.buyerId || '',
+      totalAmount: amount_total ? (amount_total / 100).toString() : '0',
+      createdAt: new Date(),
+    }
+
+    const newOrder = await createOrder(order)
+    return NextResponse.json({ message: 'OK', order: newOrder })
+  }
+
+  return new Response('', { status: 200 })
+}
