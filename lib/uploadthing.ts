b5b55f8fa349ea320d41407747eac7fa2d83a1ab
Yukio <elsie.atieno@student.moringaschool.com>
2023-12-22 11:33:16 +0300

    lib

---
+++ lib/uploadthing.ts
@@ -0,0 +1,8 @@
+// import { generateComponents } from "@uploadthing/react";
+import { generateReactHelpers } from "@uploadthing/react/hooks"; 
+import type { OurFileRouter } from "@/app/api/uploadthing/core";
+ 
+export const { useUploadThing, uploadFiles } =  generateReactHelpers<OurFileRouter>();
+
+  // export const { UploadButton, UploadDropzone, Uploader } =
+  // generateComponents<OurFileRouter>();
---
+++ lib/validator.ts
@@ -0,0 +1,14 @@
+import * as z from "zod"
+
+export const listingFormSchema = z.object({
+  title: z.string().min(3, 'Title must be at least 3 characters'),
+  description: z.string().min(3, 'Description must be at least 3 characters').max(400, 'Description must be less than 400 characters'),
+  location: z.string().min(3, 'Location must be at least 3 characters').max(400, 'Location must be less than 400 characters'),
+  imageUrl: z.string(),
+  categoryId: z.string(),
+  price: z.string(),
+  isAvailable: z.boolean(),
+})
+
+
+
