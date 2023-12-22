3a5ac11ad1617aa179a0889d232a5167d3dfe3e8
Yukio <elsie.atieno@student.moringaschool.com>
2023-12-22 10:46:44 +0300

    constants

---
+++ constants/index.ts
@@ -0,0 +1,25 @@
+export const headerLinks = [
+    {
+      label: 'Home',
+      route: '/',
+    },
+    {
+      label: 'Create Listing',
+      route: '/houses/create',
+    },
+    {
+      label: 'My Profile',
+      route: '/profile',
+    },
+  ]
+  
+  export const houseDefaultValues = {
+    title: '',
+    description: '',
+    location: '',
+    imageUrl: '',
+    categoryId: '',
+    price: '',
+    isAvailable: false,
+    url: '',
+  }
