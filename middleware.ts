93d6295a04c8de387887b2cc150d90de7bce47a9
Yukio <elsie.atieno@student.moringaschool.com>
2023-12-22 10:48:44 +0300

    estates

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { authMiddleware } from "@clerk/nextjs";
+ 
+export default authMiddleware({
+    publicRoutes: [
+        '/', '/houses/:id','/api/webhook/clerk','/api/webhook/stripe','/api/uploadthing'
+    ],
+    ignoredRoutes:[
+        '/api/webhook/clerk','/api/webhook/stripe','/api/uploadthing'
+    ]
+});
+
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
+ 
